f8244c3248b627b6b81b4930ccde5e1e135aed86
Chavan Shraddha <138581105+SHRADDHA021@users.noreply.github.com>
2024-08-29 09:21:41 +0530

    Create slip16.php

---
+++ slip16.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+
+<title>Shrirampur</title>
+
+<body style="background-color:Pink;">
+
+<h3 style="font-size:100px; color:blue;"> Shrirampur</h3>                   
+<li style="color:brown; font-style:normal;">Ram Temple</li>
+
+<li style="color:red; font-style: oblique;">Belapur Station</li>
+
+<li style="color:purple; font-style: italic;">R.b.n.b College</li>
+
+</ul>
+
+<img src="C:\Users\OMSAI\Desktop\xyz.png" width="200" height="200" align="bottom">
+</body>
+</head>
